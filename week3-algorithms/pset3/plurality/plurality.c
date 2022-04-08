68e52d7e24fe134e7a16d38d5c3a1664a0915b07
Westen0511 <xiyuanjunzhi@gmail.com>
2022-04-09 07:46:48 +0900

    Finish problem-plurality

--- week3-algorithms/pset3/plurality/plurality.c
+++ week3-algorithms/pset3/plurality/plurality.c
@@ -66,14 +66,39 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
     return false;
 }
 
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
+    int max_number_of_votes = 0;
+
+    // Set max number of votes
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > max_number_of_votes)
+        {
+            max_number_of_votes = candidates[i].votes;
+        }
+    }
+
+    // Print the winner (or winners)
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == max_number_of_votes)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+    }
     return;
 }
 
